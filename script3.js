a48106742c77352830da0cb71f25762823732b29
Przemek Krawczynski <1986krawiec1986@gmail.com>
2023-01-22 19:17:21 +0100

    function added

--- script3.js
+++ script3.js
@@ -1,23 +1,33 @@
-console.log("Witam Wszystkich!");
-
-let body = document.querySelector(".body");
-let button = document.querySelector(".button");
-let themeName = document.querySelector(".themeName");
-
-
-button.addEventListener("click", () => {
-
-    body.classList.toggle("dark");
-
-    if (body.classList.contains("dark")) {
-        themeName.innerText = "Jasne";
-    }
-    else {
-        themeName.innerText = "Ciemne";
+{
+    const welcome = () => {
+        console.log("Witam Wszystkich!");
 
     }
+    const init = () => {
+
+        const button = document.querySelector(".button");
+        button.addEventListener("click", ToggleBackground);
+    }
+    init()
+
+    const ToggleBackground = () => {
+        const themeName = document.querySelector(".themeName");
+        const body = document.querySelector(".body");
+        body.classList.toggle("dark");
+
+        if (body.classList.contains("dark")) {
+            themeName.innerText = "Jasne";
+        }
+        else {
+            themeName.innerText = "Ciemne";
+
+        }
 
 
 
+    };
+
+    welcome();
 }
-);
+
+
