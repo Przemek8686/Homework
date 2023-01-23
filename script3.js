cef2f485f01605012dbca91d34960827c7996795
Przemek Krawczynski <1986krawiec1986@gmail.com>
2023-01-23 16:00:28 +0100

    errors fixed

--- script3.js
+++ script3.js
@@ -3,13 +3,22 @@
         console.log("Witam Wszystkich!");
 
     }
-    const onChangeBackgroundCilck = () => {
+    function onChangeBackgroundCilck() {
         const themeName = document.querySelector(".themeName");
         const body = document.querySelector(".body");
         body.classList.toggle("dark");
-        themeName.innerText = body.classList.contains("dark") ? "Jasny" : "Ciemne";
-    };
 
+        if (body.classList.contains("dark")) {
+            themeName.innerText = "Jasne";
+        }
+        else {
+            themeName.innerText = "Ciemne";
+
+        }
+
+
+
+    }
     const init = () => {
 
         const button = document.querySelector(".button");
